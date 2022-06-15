e2308e1debb2e26b9f6654a041cd2877a0e7d445
巩森森 <LightHouseGss@gmail.com>
2022-06-15 21:56:51 +0800

    add - 添加Base基础库、Http网络请求库、TrafficStatistic流量统计库

---
+++ Lib_Location/src/main/java/com/power/location/LocationUtils.kt
@@ -0,0 +1,208 @@
+package com.power.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.text.TextUtils
+import androidx.core.app.ActivityCompat
+
+/**
+ * 作者：Gongsensen
+ * 日期：2022/6/15
+ * 说明：
+ */
+object LocationUtils {
+
+    private const val REFRESH_TIME = 300L
+    private const val METER_POSITION = 0.0f
+    private var mLocationListener: ILocationListener? = null
+    private var listener: LocationListener = MyLocationListener()
+
+    private class MyLocationListener : LocationListener {
+        override fun onLocationChanged(location: Location) { //定位改变监听
+            if (mLocationListener != null) {
+                mLocationListener?.onSuccessLocation(location)
+            }
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) { //定位状态监听
+        }
+
+        override fun onProviderEnabled(provider: String) { //定位状态可用监听
+        }
+
+        override fun onProviderDisabled(provider: String) { //定位状态不可用监听
+        }
+    }
+
+    /**
+     * GPS获取定位方式
+     */
+    fun getGPSLocation(context: Context): Location? {
+        var location: Location? = null
+        val manager = getLocationManager(context)
+        //高版本的权限检查
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return null
+        }
+        if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) { //是否支持GPS定位
+            //获取最后的GPS定位信息，如果是第一次打开，一般会拿不到定位信息，一般可以请求监听，在有效的时间范围可以获取定位信息
+            location = manager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+        return location
+    }
+
+    /**
+     * network获取定位方式
+     */
+    fun getNetWorkLocation(context: Context): Location? {
+        var location: Location? = null
+        val manager = getLocationManager(context)
+        //高版本的权限检查
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return null
+        }
+        if (manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) { //是否支持Network定位
+            //获取最后的network定位信息
+            location = manager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+        }
+        return location
+    }
+
+    /**
+     * 获取最好的定位方式
+     */
+    fun getBestLocation(context: Context, criteria: Criteria = getDefaultCriteria()): Location? {
+        var location: Location? = null
+        val manager = getLocationManager(context)
+        val provider: String? = manager.getBestProvider(criteria, true)
+        if (TextUtils.isEmpty(provider)) {
+            //如果找不到最适合的定位，使用network定位
+            location = getNetWorkLocation(context)
+        } else {
+            //高版本的权限检查
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return null
+            }
+            //获取最适合的定位方式的最后的定位权限
+            if (!provider.isNullOrEmpty()) {
+                location = manager.getLastKnownLocation(provider)
+            }
+        }
+        return location
+    }
+
+    /**
+     * 定位监听
+     */
+    fun addLocationListener(
+        context: Context,
+        provider: String = LocationManager.GPS_PROVIDER,
+        locationListener: ILocationListener?
+    ) {
+        addLocationListener(
+            context,
+            provider,
+            REFRESH_TIME,
+            METER_POSITION,
+            locationListener
+        )
+    }
+
+    /**
+     * 定位监听
+     */
+    fun addLocationListener(
+        context: Context,
+        provider: String,
+        time: Long,
+        meter: Float,
+        locationListener: ILocationListener?
+    ) {
+        if (locationListener != null) {
+            mLocationListener = locationListener
+        }
+        if (listener == null) {
+            listener = MyLocationListener()
+        }
+        val manager = getLocationManager(context)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        manager.requestLocationUpdates(provider, time, meter, listener)
+    }
+
+    /**
+     * 获取默认的Criteria
+     */
+    private fun getDefaultCriteria(): Criteria {
+        val c = Criteria()//Criteria类是设置定位的标准信息（系统会根据你的要求，匹配最适合你的定位供应商），一个定位的辅助信息的类
+        c.powerRequirement = Criteria.POWER_HIGH//设置低耗电
+        c.isAltitudeRequired = true//设置需要海拔
+        c.bearingAccuracy = Criteria.ACCURACY_HIGH//设置COARSE精度标准
+        c.accuracy = Criteria.ACCURACY_COARSE//设置低精度
+        return c
+    }
+
+    /**
+     * 取消定位监听
+     */
+    fun unRegisterListener(context: Context) {
+        if (listener != null) {
+            val manager = getLocationManager(context)
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            //移除定位监听
+            manager.removeUpdates(listener)
+        }
+    }
+
+    private fun getLocationManager(context: Context): LocationManager {
+        return context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    }
+
+    /**
+     * 自定义接口
+     */
+    interface ILocationListener {
+        fun onSuccessLocation(location: Location?)
+    }
+}
---
+++ app/src/main/java/com/power/land/location/SampleLocationActivity.kt
@@ -0,0 +1,189 @@
+package com.power.land.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationManager
+import android.os.Build
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.blankj.utilcode.util.LogUtils
+import com.power.land.R
+import com.power.location.LocationUtils
+
+/**
+ * 作者：Gongsensen
+ * 日期：2022/6/15
+ * 说明：
+ */
+class SampleLocationActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "SampleLocationActivity"
+    }
+
+    private var flag = false
+
+    private var locationGetGPS: Button? = null
+    private var locationGetNetGPS: Button? = null
+    private var locationGetBestGPS: Button? = null
+    private var locationShowTv: TextView? = null
+
+    private var tvTextList = mutableListOf<String>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_location)
+        locationGetGPS = findViewById(R.id.locationGetGPS)
+        locationGetNetGPS = findViewById(R.id.locationGetNetGPS)
+        locationGetBestGPS = findViewById(R.id.locationGetBestGPS)
+        locationShowTv = findViewById(R.id.locationShowTv)
+
+        locationGetGPS?.setOnClickListener {
+            getGPSLocation()
+        }
+
+        locationGetNetGPS?.setOnClickListener {
+            getNetworkLocation()
+        }
+
+        locationGetBestGPS?.setOnClickListener {
+            getBestLocation()
+        }
+        initLocationListener()
+    }
+
+    /**
+     * 通过GPS获取定位信息
+     */
+    private fun getGPSLocation() {
+        val gps = LocationUtils.getGPSLocation(this)
+        if (gps == null) {
+            showTv("getGPSLocation gps location is null")
+            LogUtils.d("$TAG getGPSLocation gps location is null")
+        } else {
+            showTv("getGPSLocation lat==${gps.latitude} lng==${gps.longitude}")
+            LogUtils.d("$TAG getGPSLocation lat==${gps.latitude} lng==${gps.longitude}")
+        }
+    }
+
+    /**
+     * 通过网络等获取定位信息
+     */
+    private fun getNetworkLocation() {
+        val net = LocationUtils.getNetWorkLocation(this)
+        if (net == null) {
+            showTv("getNetworkLocation gps location is null")
+            LogUtils.d("$TAG getNetworkLocation gps location is null")
+        } else {
+            showTv("getNetworkLocation lat==${net.latitude} lng==${net.longitude}")
+            LogUtils.d("$TAG getNetworkLocation lat==${net.latitude} lng==${net.longitude}")
+        }
+    }
+
+    /**
+     * 采用最好的方式获取定位信息
+     */
+    private fun getBestLocation() {
+        val best = LocationUtils.getBestLocation(this)
+        if (best == null) {
+            showTv("getBestLocation gps location is null")
+            LogUtils.d("$TAG getBestLocation gps location is null")
+        } else {
+            showTv("getNetworkLocation lat==${best.latitude} lng==${best.longitude}")
+            LogUtils.d("$TAG getNetworkLocation lat==${best.latitude} lng==${best.longitude}")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initPermission();//针对6.0以上版本做权限适配
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        initLocationListener()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LocationUtils.unRegisterListener(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            flag =
+                grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED
+            if (flag) {
+                initLocationListener()
+            }
+        }
+    }
+
+    private fun initLocationListener() {
+        LocationUtils.addLocationListener(
+            this,
+            LocationManager.GPS_PROVIDER,
+            object : LocationUtils.ILocationListener {
+                override fun onSuccessLocation(location: Location?) {
+                    if (location != null) {
+                        showTv("onSuccessLocation lat==${location.latitude} lng==${location.longitude}")
+                        LogUtils.d("$TAG onSuccessLocation lat==${location.latitude} lng==${location.longitude}")
+                    } else {
+                        showTv("onSuccessLocation gps location is null")
+                        LogUtils.d("$TAG onSuccessLocation gps location is null")
+                    }
+                }
+            })
+
+    }
+
+    private fun initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //检查权限
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                //请求权限
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    1
+                )
+            } else {
+                flag = true
+            }
+        } else {
+            flag = true
+        }
+    }
+
+    private fun showTv(string: String) {
+        if (tvTextList.size >= 15) {
+            tvTextList.clear()
+            locationShowTv?.text = ""
+        }
+        locationShowTv?.append("$string \n")
+        tvTextList.add(string)
+
+
+    }
+}
